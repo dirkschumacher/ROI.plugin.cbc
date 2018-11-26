453a88492b9e7e2b42c997c807f09c7ab84bcb71
hugolarzabal <40272209+hugolarzabal@users.noreply.github.com>
2018-11-26 16:43:28 +0100

    tests to make sure lower column bounds works

--- tests/testthat/test-plugin.R
+++ tests/testthat/test-plugin.R
@@ -47,3 +47,31 @@ test_that("supports equality constraints", {
   expect_equal(120, result$objval)
   expect_equal(60, result$solution)
 })
+
+test_that("can solve simple IP problem with lower col bounds and lower columns bounds", {
+  IP <- ROI::OP(c(2, 4, 3),
+                ROI::L_constraint(
+                  L = matrix(c(3, 2, 1, 4, 1, 3, 2, 2, 2), nrow = 3),
+                  dir = c("<=", ">=", "<="),
+                  rhs = c(60, 40, 80)),
+                types = c("I", "I", "B"),
+                max = TRUE )
+  bounds(IP) <- V_bound(li=1:3, lb=c(20,-1,1))
+  result <- ROI::ROI_solve(IP, "cbc")
+  expect_equal(39, result$objval)
+  expect_equal(c(20, -1, 1), result$solution)
+})
+
+test_that("can solve simple lp problem with lower row bounds and lower columns bounds", {
+  LP <- ROI::OP(c(2, 4, 3),
+                ROI::L_constraint(
+                  L = matrix(c(3, 2, 1, 4, 1, 3, 2, 2, 2), nrow = 3),
+                  dir = c("<=", ">=", "<="),
+                  rhs = c(60, 40, 80)),
+                max = TRUE )
+  bounds(LP) <- V_bound(li=1:3, lb=c(10,0,0))
+  result <- ROI::ROI_solve(LP, "cbc")
+  expect_equal(65, result$objval)
+  expect_equal(c(10, 0, 15), result$solution)
+})
+
